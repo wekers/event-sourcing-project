d949d4b6684df447e2db4e1b3f1ae65058ce5213
wekers <fernando@wekers.org>
2025-09-08 10:39:51 -0300

    refactor to better on flyway

--- query-service/src/main/java/com/example/eventsourcing/query/application/infrastructure/outbox/OutboxPendingAck.java
+++ query-service/src/main/java/com/example/eventsourcing/query/application/infrastructure/outbox/OutboxPendingAck.java
@@ -9,19 +9,23 @@ import java.util.UUID;
 
 
 @Entity
-@Getter @Setter
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class OutboxPendingAck {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // casa com "IDENTITY" do Flyway
     private Long id;
 
+    @Column(name = "outbox_event_id", nullable = false, columnDefinition = "UUID")
     private UUID outboxEventId;
 
+    @Column(name = "created_at", nullable = false, updatable = false)
     private Instant createdAt = Instant.now();
 
+    @Column(name = "retry_count", nullable = false)
     private int retryCount = 0;
 
     public OutboxPendingAck(UUID outboxEventId) {
