c76a22be264f829887497ad2009c80bb5fd2af4c
wekers <fernando@wekers.org>
2025-09-25 18:07:31 -0300

    5s ok

--- docker/debezium/init.sh
+++ docker/debezium/init.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "⏳ Waiting for Kafka Connect..."
-sleep 10
+sleep 5
 curl -X POST http://debezium-connect:8083/connectors \
   -H "Content-Type: application/json" \
   -d @/kafka/config/register-postgres.json \
